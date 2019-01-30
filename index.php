c59a149f77c220b46682050d1f9ba62cbb666900
drpdishant <drpdishant@gmail.com>
2019-01-30 19:03:39 +0530

    my commit

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-Hello World!
+<?php
+include 'mysqli_object_orinted/connect_db.php';
+
+echo "<br><br>";
+
+include 'mysqli_procedural/connect_db.php';
+echo "<br><br>";
+
+include 'pdo/connect_db.php';
+
+echo "<br><br>";
+?>
