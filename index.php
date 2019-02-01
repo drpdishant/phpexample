049e2951dc570addb5ecb5bba2a253cae1cd968c
drpdishant <drpdishant@gmail.com>
2019-02-01 19:28:08 +0530

    Select Data

--- index.php
+++ index.php
@@ -9,5 +9,6 @@ include $php_dir."create_table.php";
 echo '<br>';
 include $php_dir."add_item.php";
 echo '<br>';
-
+include $php_dir."select_data.php";
+echo '<br>';
 ?>
---
+++ mysqli/select_data.php
@@ -0,0 +1,28 @@
+<?php
+$db_host = "localhost";
+$db_root_user = "root";
+$db_root_password = "";
+$db_name = "db_oo";
+
+$conn = new mysqli($db_host, $db_root_user, $db_root_password, $db_name);
+
+if($conn->connect_error)
+{
+    echo("Connection Failed: " . $conn->connect_error);
+}
+$sql = "select TaskName, TaskDescription, date_time FROM TaskList";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+    echo "Task Name   | Task Description                          | Time Stamp";
+    while ($row = $result->fetch_assoc())
+    {
+        echo $row["TaskName"]." | ".$row["TaskDescription"] . "  |  " . $row["date_time"]. "<br>"; 
+    }
+}
+else
+{
+    echo("0 Results");
+}
+$conn->close();
+?>
