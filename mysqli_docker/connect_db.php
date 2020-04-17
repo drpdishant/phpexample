a36f4ff141506dbac8fdd4d25edca4202e39838f
Dishant Pandya <drpdishant@gmail.com>
2020-04-17 19:27:44 +0530

    Update connect_db.php

--- mysqli_docker/connect_db.php
+++ mysqli_docker/connect_db.php
@@ -1,7 +1,8 @@
 <?php
-$db_host = "db";
-$db_root_user = "root";
-$db_root_password = "rootuser";
+$db_host = "MYSQL_HOSTNAME";
+$db_root_user = "MYSQL_ROOT_USER";
+$db_root_password = "MYSQL_ROOT_PASSWORD";
+$db_name = "MYSQL_DATABASE";
 
 // Create connection
 
