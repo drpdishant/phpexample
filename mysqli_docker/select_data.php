fb0fa948c50eff4f020b53edf3074fb8985b9ca4
Neeraj <neeraj.j.addweb@gmail.com>
2020-04-18 17:05:02 +0530

    configure db with env

--- mysqli_docker/select_data.php
+++ mysqli_docker/select_data.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "MYSQL_HOSTNAME";
-$db_root_user = "MYSQL_ROOT_USER";
-$db_root_password = "MYSQL_ROOT_PASSWORD";
-$db_name = "MYSQL_DATABASE";
+$db_host = getenv('MYSQL_HOSTNAME');
+$db_root_user = getenv('MYSQL_ROOT_USER');
+$db_root_password = getenv('MYSQL_ROOT_PASSWORD');
+$db_name = getenv('MYSQL_DATABASE');
 
 $conn = new mysqli($db_host, $db_root_user, $db_root_password, $db_name);
 
